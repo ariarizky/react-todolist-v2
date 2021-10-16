0d91b496ed2e59fb7b96258aa8e0709f19825176
ariarizky <58224363+ariarizky@users.noreply.github.com>
2021-10-16 10:24:01 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn main-black-color">Add</button>
+          <h1 className="header-title">Todo List</h1>
+          <button id="clear-btn" className="header-btn main-red-color">
+            Clear
+          </button>
+        </section>
+        <section className="add">
+          <form action="" className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn main-black-color">Add</button>
+          </form>
+        </section>
+        <section className="todos"></section>
+      </div>
+    </div>
+  );
+}
