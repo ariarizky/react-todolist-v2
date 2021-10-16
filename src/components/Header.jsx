d9fe50d817f91999c38f6e7fc853b6da8d748160
ariarizky <58224363+ariarizky@users.noreply.github.com>
2021-10-16 11:17:38 +0700

    membuat Header, TodoForm, Todos Comoponent

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="header">
+      <button className="header-btn main-black-color">Add</button>
+      <h1 className="header-title">Todo List</h1>
+      <button id="clear-btn" className="header-btn main-red-color">
+        Clear
+      </button>
+    </section>
+  );
+};
+export default Header;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Todos = () => {
+  return <section className="todos"></section>;
+};
+export default Todos;
