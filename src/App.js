d4094afd45af993756e23de635d60df60d0e402c
ariarizky <58224363+ariarizky@users.noreply.github.com>
2021-10-16 10:57:41 +0700

    membuat Paper component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import "./styles.css";
+import Paper from "./components/Paper";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn main-black-color">Add</button>
-          <h1 className="header-title">Todo List</h1>
-          <button id="clear-btn" className="header-btn main-red-color">
-            Clear
-          </button>
-        </section>
-        <section className="add">
-          <form action="" className="add-form">
-            <input type="text" className="add-input" />
-            <button className="add-btn main-black-color">Add</button>
-          </form>
-        </section>
-        <section className="todos"></section>
-      </div>
-    </div>
+    <Paper>
+      <section className="header">
+        <button className="header-btn main-black-color">Add</button>
+        <h1 className="header-title">Todo List</h1>
+        <button id="clear-btn" className="header-btn main-red-color">
+          Clear
+        </button>
+      </section>
+      <section className="add">
+        <form action="" className="add-form">
+          <input type="text" className="add-input" />
+          <button className="add-btn main-black-color">Add</button>
+        </form>
+      </section>
+      <section className="todos"></section>
+    </Paper>
   );
 }
