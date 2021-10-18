b1f9aa5e987b28e0e6153c22139093ceeeed0216
ariarizky <58224363+ariarizky@users.noreply.github.com>
2021-10-18 10:00:11 +0700

    menambah Todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,45 @@
 import React from "react";
+import Todo from "./Todo";
 
 const Todos = () => {
-  return <section className="todos"></section>;
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    }
+  ];
+  return (
+    <section className="todos">
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
+    </section>
+  );
 };
 export default Todos;
