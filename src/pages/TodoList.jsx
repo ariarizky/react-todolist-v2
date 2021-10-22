414d493d45f50e20df9b1b2ae179007724097b86
ariarizky <58224363+ariarizky@users.noreply.github.com>
2021-10-22 14:30:45 +0700

    update todolist

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,26 +1,94 @@
-import React, { useState } from "react";
+import React from "react";
 
-import Paper from "../components/Paper";
-import Header from "../components/Header";
-import TodoForm from "../components/TodoForm";
-import Todos from "../components/Todos";
+import Paper from "../components/paper/PaperClass";
+import Header from "../components/header/HeaderClass";
+import TodoForm from "../components/todoForm/TodoFormClass";
+import Todos from "../components/todos/TodosClass";
 
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [showAdd, setShowAdd] = useState(false);
-  const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+import Container from "../layout/ContainerClass";
 
-    setTodos(addedTodo);
+class TodoList extends React.Component {
+  state = {
+    todos: [],
+    showAdd: false
   };
-  const showAddToggle = () => setShowAdd(!showAdd);
 
-  return (
-    <Paper>
-      <Header showAddToggle={showAddToggle} />
-      <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
-    </Paper>
-  );
-};
+  componentDidMount() {
+    const todosStateLocalStorage =
+      JSON.parse(localStorage.getItem("todos")) || [];
+
+    const showAddStateLocalStorage =
+      JSON.parse(localStorage.getItem("showAdd")) || false;
+
+    this.setState({
+      todos: todosStateLocalStorage,
+      showAdd: showAddStateLocalStorage
+    });
+  }
+
+  componentDidUpdate() {
+    const { todos, showAdd } = this.state;
+    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem("showAdd", JSON.stringify(showAdd));
+  }
+
+  addTodo = (value) => {
+    if (this.state.todos.length < 10) {
+      const addedTodos = [
+        ...this.state.todos,
+        { text: value, isCompleted: false }
+      ];
+
+      this.setState({
+        todos: addedTodos
+      });
+    } else {
+      alert("Only 10 todos is allowed!");
+    }
+  };
+
+  completeTodo = (index) => {
+    const addedTodos = [...this.state.todos];
+    addedTodos[index].isCompleted = !addedTodos[index].isCompleted;
+
+    this.setState({
+      todos: addedTodos
+    });
+  };
+
+  clearTodos = () => {
+    if (this.state.showAdd) {
+      alert("Finish add todo before clear");
+      return;
+    }
+
+    this.setState({
+      todos: []
+    });
+  };
+
+  showAddToggle = () =>
+    this.setState((prevState) => ({ showAdd: !prevState.showAdd }));
+
+  render() {
+    return (
+      <Paper>
+        <Container
+          flexDirection="column"
+          justifyContent="space-between"
+          height="100%"
+        >
+          <Header
+            showAddToggle={this.showAddToggle}
+            showAdd={this.state.showAdd}
+            clearTodos={this.clearTodos}
+          />
+          <TodoForm addTodo={this.addTodo} showAdd={this.state.showAdd} />
+          <Todos todos={this.state.todos} completeTodo={this.completeTodo} />
+        </Container>
+      </Paper>
+    );
+  }
+}
+
 export default TodoList;
